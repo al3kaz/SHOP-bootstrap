124243bb1f1b9d5dd81842bcf0e9dfe5ea3a05d9
Aleksander Kazmierczak <aleksander.kazmierczak@gmail.com>
2020-09-15 20:47:39 +0200

    webpack configuration

--- webpack.dev.js
+++ webpack.dev.js
@@ -45,7 +45,7 @@ module.exports = {
     },
 
     plugins: [
-        new HtmlWebpackPlugin({template: './index.html'}),
+        new HtmlWebpackPlugin({ template: './index.html' }),
         new webpack.HotModuleReplacementPlugin(),
     ]
 };
